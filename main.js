3cd9986b965a44c326a1aa0fd554ff4d23fa651c
fewk <55815192+fewk@users.noreply.github.com>
2024-11-06 09:33:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ async function tts(text, lang, options = {}) {
     const { config, utils } = options;
     const { tauriFetch } = utils;
     let { requestPath } = config;
-    if (requestPath === undefined || requestPath.length === 0) {
-        requestPath = "lingva.pot-app.com"
-    }
-    if (!requestPath.startsWith('http')) {
-        requestPath = 'https://' + requestPath;
-    }
-    const res = await tauriFetch(`${requestPath}/api/v1/audio/${lang}/${encodeURIComponent(text)}`);
+
+    // 如果未定义 requestPath 或为空，则设置为新 URL
+    requestPath = "https://t.leftsite.cn/tts";
+
+    // 构建 URL，注意 URL 中的参数结构与示例请求一致
+    const url = `${requestPath}?t=${encodeURIComponent(text)}&v=${lang}&r=0&p=0&o=audio-24khz-48kbitrate-mono-mp3`;
+
+    const res = await tauriFetch(url);
 
     if (res.ok) {
         let result = res.data;
